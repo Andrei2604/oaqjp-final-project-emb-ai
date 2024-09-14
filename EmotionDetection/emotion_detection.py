6365334d1edf421a16c5852acaea981635cc7ac8
Andrei2604 <andrejporodin@gmail.com>
2024-09-14 05:34:02 -0400

    Task 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,19 +6,28 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     obj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = obj, headers = header)
-    formatted_text = json.loads(response.text)
-    anger_score = formatted_text["emotionPredictions"][0]["emotion"]["anger"]
-    disgust_score = formatted_text["emotionPredictions"][0]["emotion"]["disgust"]
-    fear_score = formatted_text["emotionPredictions"][0]["emotion"]["fear"]
-    joy_score = formatted_text["emotionPredictions"][0]["emotion"]["joy"]
-    sadness_score = formatted_text["emotionPredictions"][0]["emotion"]["sadness"]
+    if response.status_code == 400:
+        result = {'anger' : None,
+        'disgust' : None,
+        'fear' : None, 
+        'joy' : None,
+        'sadness' : None,
+        'dominant_emotion' : None}
+    else:
+        formatted_text = json.loads(response.text)
+        anger_score = formatted_text["emotionPredictions"][0]["emotion"]["anger"]
+        disgust_score = formatted_text["emotionPredictions"][0]["emotion"]["disgust"]
+        fear_score = formatted_text["emotionPredictions"][0]["emotion"]["fear"]
+        joy_score = formatted_text["emotionPredictions"][0]["emotion"]["joy"]
+        sadness_score = formatted_text["emotionPredictions"][0]["emotion"]["sadness"]
     
-    result = {'anger' : anger_score,
-    'disgust' : disgust_score,
-    'fear' : fear_score, 
-    'joy' : joy_score,
-    'sadness' : sadness_score}
+        result = {'anger' : anger_score,
+        'disgust' : disgust_score,
+        'fear' : fear_score, 
+        'joy' : joy_score,
+        'sadness' : sadness_score}
     
-    dominant_emotion = max(result, key = result.get)
-    result['dominant_emotion'] = dominant_emotion
+        dominant_emotion = max(result, key = result.get)
+        result['dominant_emotion'] = dominant_emotion
+        
     return result
--- server.py
+++ server.py
@@ -17,6 +17,9 @@ def sent_detector():
     joy = result["joy"]
     sadness = result["sadness"]
     dominant_emotion = result["dominant_emotion"]
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
     return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
 if __name__ == "__main__":
